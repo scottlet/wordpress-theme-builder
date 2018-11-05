73832a9b4bd2d1cf238f98f87db8b75f323eb662
Scott van Looy <scott@quul.net>
2018-11-05 07:21:32 +0100

    Multiple entries. Templates

--- gulp-tasks/browserify.js
+++ gulp-tasks/browserify.js
@@ -2,6 +2,7 @@
 
 const browserify = require('browserify');
 const CONSTS = require('./CONSTS');
+const glob = require('glob');
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
 const gulpLivereload = require('gulp-livereload');
@@ -17,50 +18,73 @@ const watchify = require('watchify');
 
 const isDev = CONSTS.NODE_ENV !== 'production';
 
-const options = {
-    entries:  [].concat(CONSTS.JS_ENTRY),
-    cache: {},
-    packageCache: {},
-    paths: [
-        './src/js/modules'
-    ]
-};
+const entries = glob.sync(CONSTS.JS_SRC + '*.js');
 
-if (isDev) {
-    options.plugin = [watchify];
-}
+console.log(entries);
 
-const b = browserify(options);
+function addToBrowserify(entry) {
+    const options = {
+        entries:  [entry],
+        cache: {},
+        packageCache: {},
+        paths: [
+            './src/js/modules'
+        ]
+    };
 
-function doLR() {
-    if (process.env.OVERRIDE_LR === 'true') {
-        return false;
+    const name = entry.replace('$name', CONSTS.NAME).replace('$version', CONSTS.VERSION)
+        .replace(/.*\/([\w$\-.]+).js/, '$1');
+
+    console.log('name', name);
+
+    const uglifyOptions = {
+        compress: {
+            drop_console: true //eslint-disable-line
+        }
+    };
+
+    if (isDev) {
+        options.plugin = [watchify];
+        delete uglifyOptions.compress.drop_console;
     }
 
-    return isDev;
+    const b = browserify(options);
+
+    function doLR() {
+        if (process.env.OVERRIDE_LR === 'true') {
+            return false;
+        }
+
+        return isDev;
+    }
+
+    function bundle() {
+        return b.bundle()
+            .pipe(gulpPlumber({errorHandler: gulpNotify.onError(error => `JS Bundle Error: ${error.message}`)}))
+            .pipe(vinylSourceStream(name + CONSTS.JS_OUTPUT))
+            .pipe(vinylBuffer())
+            .pipe(gulpReplace('$$oldMobile$$', CONSTS.BREAKPOINTS.OLD_MOBILE))
+            .pipe(gulpReplace('$$mobile$$', CONSTS.BREAKPOINTS.MOBILE))
+            .pipe(gulpReplace('$$smalltablet$$', CONSTS.BREAKPOINTS.SMALL_TABLET))
+            .pipe(gulpReplace('$$tablet$$', CONSTS.BREAKPOINTS.TABLET))
+            .pipe(gulpReplace('$$smalldesktop$$', CONSTS.BREAKPOINTS.SMALL_DESKTOP))
+            .pipe(gulpSourcemaps.init({loadMaps: true}))
+            .pipe(gulpUglify(uglifyOptions))
+            .pipe(gulpIf(isDev, gulpSourcemaps.write()))
+            .pipe(gulp.dest(CONSTS.JS_DEST))
+            .pipe(gulpIf(doLR(), gulpLivereload({
+                port: CONSTS.LIVERELOAD_PORT
+            })));
+    }
+
+    b.on('update', bundle);
+    b.on('log', fancyLog);
+    b.on('error', fancyLog);
+    bundle();
 }
 
 function bundle() {
-    return b.bundle()
-        .pipe(gulpPlumber({errorHandler: gulpNotify.onError(error => `JS Bundle Error: ${error.message}`)}))
-        .pipe(vinylSourceStream(CONSTS.JS_OUTPUT))
-        .pipe(vinylBuffer())
-        .pipe(gulpReplace('$$oldMobile$$', CONSTS.BREAKPOINTS.OLD_MOBILE))
-        .pipe(gulpReplace('$$mobile$$', CONSTS.BREAKPOINTS.MOBILE))
-        .pipe(gulpReplace('$$smalltablet$$', CONSTS.BREAKPOINTS.SMALL_TABLET))
-        .pipe(gulpReplace('$$tablet$$', CONSTS.BREAKPOINTS.TABLET))
-        .pipe(gulpReplace('$$smalldesktop$$', CONSTS.BREAKPOINTS.SMALL_DESKTOP))
-        .pipe(gulpSourcemaps.init({loadMaps: true}))
-        .pipe(gulpUglify())
-        .pipe(gulpIf(isDev, gulpSourcemaps.write()))
-        .pipe(gulp.dest(CONSTS.JS_DEST))
-        .pipe(gulpIf(doLR(), gulpLivereload({
-            port: CONSTS.LIVERELOAD_PORT
-        })));
+    entries.forEach(addToBrowserify);
 }
-
-b.on('update', bundle);
-b.on('log', fancyLog);
-b.on('error', fancyLog);
 
 gulp.task('browserify', bundle);
