1cc9ad339950ee9076ad4da4f5f94bcb21fad90c
Scott van Looy <scott@quul.net>
2019-01-09 05:07:17 +0100

    Move to Gulp 4

--- gulp-tasks/default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-
-gulp.task('default', ['server']);
-
-gulp.task('build', ['check', 'eslint', 'clean', 'copy', 'sass', 'browserify']);
-gulp.task('deploy', ['buildtoprod']);
-gulp.task('buildtoprod', ['build', 'copydeploy', 'zip']);
